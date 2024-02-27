20adf9151a7403b8b3807c15f0f2f929b452019c
VishnuSangammanavar <vishnusangammanavar25@gmail.com>
2024-02-27 14:11:02 +0530

    Update file

--- src/App.js
+++ src/App.js
@@ -15,30 +15,31 @@ import Academyalliances from './pages/Academyalliances';
 import News from './pages/News';
 import Events from './pages/Events';
 import Workshops from './pages/Workshops';
+import Layout from './pages/Layout';
 import Gallery from './pages/Gallery';
-import Header from './Components/Header';
 
 function App() {
   return (
     <>
       <Topbar />
-      <Header />
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/about' element={<About />} />
-        <Route exact path='/about/kle-society' element={<Klesociety />} />
-        <Route exact path='/about/kle-bca' element={<Klebca />} />
-        {/* <Route exact path='/academics/bca-syllabus' element={<Bcasyllabus />} /> */}
-        <Route exact path='/academics/specializations' element={<Specializations />} />
-        <Route exact path='/academics/academy-alliances' element={<Academyalliances />} />
-        <Route exact path='/placements' element={<Placements />} />
-        <Route exact path='/faculty' element={<Faculty />} />
-        <Route exact path='/happenings/news' element={<News />} />
-        <Route exact path='/happenings/events' element={<Events />} />
-        <Route exact path='/happenings/workshops' element={<Workshops />} />
-        <Route exact path='/gallery' element={<Gallery />} />
-        <Route exact path='/contact' element={<Contact />} />
-        <Route path='*' element={<Error />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route exact path='about' element={<About />} />
+          <Route exact path='about/kle-society' element={<Klesociety />} />
+          <Route exact path='about/kle-bca' element={<Klebca />} />
+          {/* <Route exact path='academics/bca-syllabus' element={<Bcasyllabus />} /> */}
+          <Route exact path='academics/specializations' element={<Specializations />} />
+          <Route exact path='academics/academy-alliances' element={<Academyalliances />} />
+          <Route exact path='placements' element={<Placements />} />
+          <Route exact path='faculty' element={<Faculty />} />
+          <Route exact path='happenings/news' element={<News />} />
+          <Route exact path='happenings/events' element={<Events />} />
+          <Route exact path='happenings/workshops' element={<Workshops />} />
+          <Route exact path='gallery' element={<Gallery />} />
+          <Route exact path='contact' element={<Contact />} />
+          <Route path='*' element={<Error />} />
+        </Route>
       </Routes>
       <Footer />
     </>
