6d99fd2b9cfeaec63e0965160dcec116dd41dbad
VishnuSangammanavar <vishnusangammanavar25@gmail.com>
2024-02-27 14:38:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -23,21 +23,21 @@ function App() {
     <>
       <Topbar />
       <Routes>
-        <Route path='/rlsbca' element={<Layout />}>
+        <Route path='/rlsbca/' element={<Layout />}>
           <Route index element={<Home />} />
-          <Route exact path='about' element={<About />} />
-          <Route exact path='about/kle-society' element={<Klesociety />} />
-          <Route exact path='about/kle-bca' element={<Klebca />} />
-          {/* <Route exact path='academics/bca-syllabus' element={<Bcasyllabus />} /> */}
-          <Route exact path='academics/specializations' element={<Specializations />} />
-          <Route exact path='academics/academy-alliances' element={<Academyalliances />} />
-          <Route exact path='placements' element={<Placements />} />
-          <Route exact path='faculty' element={<Faculty />} />
-          <Route exact path='happenings/news' element={<News />} />
-          <Route exact path='happenings/events' element={<Events />} />
-          <Route exact path='happenings/workshops' element={<Workshops />} />
-          <Route exact path='gallery' element={<Gallery />} />
-          <Route exact path='contact' element={<Contact />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/about/kle-society' element={<Klesociety />} />
+          <Route exact path='/about/kle-bca' element={<Klebca />} />
+          {/* <Route exact path='/academics/bca-syllabus' element={<Bcasyllabus />} /> */}
+          <Route exact path='/academics/specializations' element={<Specializations />} />
+          <Route exact path='/academics/academy-alliances' element={<Academyalliances />} />
+          <Route exact path='/placements' element={<Placements />} />
+          <Route exact path='/faculty' element={<Faculty />} />
+          <Route exact path='/happenings/news' element={<News />} />
+          <Route exact path='/happenings/events' element={<Events />} />
+          <Route exact path='/happenings/workshops' element={<Workshops />} />
+          <Route exact path='/gallery' element={<Gallery />} />
+          <Route exact path='/contact' element={<Contact />} />
           <Route path='*' element={<Error />} />
         </Route>
       </Routes>
