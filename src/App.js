6c1c70b5d01a0d4a733ed4a52854034b7f0f1104
VishnuSangammanavar <vishnusangammanavar25@gmail.com>
2024-02-27 14:15:24 +0530

    Update file

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <Topbar />
       <Routes>
         <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route exact path='rlsbca/' element={<Home />} />
           <Route exact path='about' element={<About />} />
           <Route exact path='about/kle-society' element={<Klesociety />} />
           <Route exact path='about/kle-bca' element={<Klebca />} />
